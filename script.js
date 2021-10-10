a1991402f29d7244f2cf487e0c90c9801fe07598
rorozoamuhammad <ilhammoch2006@gmail.com>
2021-10-10 17:13:29 +0700

    menambahkan game

---
+++ script.js
@@ -0,0 +1,48 @@
+var repeat = true
+
+while ( repeat ) { 
+	alert ( "selamat datang di game tebak angka \ntebak angka dari 1 - 5 \ndan komputer akan membandingkkan nya" );
+	alert ( "percobaan hanya 1 kali jadi cobalah keberutangan mu");
+
+	// user
+	var user = prompt("masukkan angka yang anda pilih 1-5 : ");
+	 
+	// komputer
+	var cop = Math.random();
+	if ( cop < 0.34 ) {
+		cop = "1";
+	}
+	else if ( cop < 0.44) {
+		cop = "2";
+	}
+	else if ( cop < 0.54) {
+		cop = "3";
+	}
+	else if ( cop >= 0.54 && cop < 0.64 ) {
+		cop = "4";
+	}
+	else {
+		cop = "5";
+	}
+
+	// aturan main
+	var hasil = "";
+
+	if ( user === cop ) {
+		hasil = "BERHASIL";
+	}
+	else if ( user != cop ) {
+		hasil = "GAGAL COBA LAGI SEMANGAT";
+	}
+	else {
+		alert ("input salah coba lagi")
+	}
+
+	// hasil
+	alert ("pilihan mu adalah " + user + "\ndan pilihan computer adalah " + cop + "\nmaka kamu " + hasil );
+
+	// ulang
+	repeat = confirm("seru?, kalo seru main lagi yuk");
+}
+
+alert ( " yah udahan kalo lagi bosen main dengan ku yuk kalian bisa ketik website yang di atas ");
